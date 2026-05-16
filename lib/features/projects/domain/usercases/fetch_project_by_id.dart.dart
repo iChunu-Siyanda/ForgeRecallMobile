4bdde523887be564cf1afa37904eed0fae6e343a
Siyanda Mchunu <mchunusiyanda225@gmail.com>
2026-05-16 08:56:29 -0700

    Add Stream to GetProjects and ProjectEntity to fetchById

--- lib/features/projects/domain/usercases/fetch_project_by_id.dart.dart
+++ lib/features/projects/domain/usercases/fetch_project_by_id.dart.dart
@@ -1,10 +1,11 @@
+import 'package:forge_recall/features/projects/domain/entities/project_entity.dart';
 import 'package:forge_recall/features/projects/domain/repositories/project_repository.dart';
 
 class FetchProjectById {
   final ProjectRepository repository;
   FetchProjectById(this.repository);
 
-  Future<void> call(String projectId) async {
-    await repository.fetchProjectById(projectId);
+  Future<ProjectEntity?> call(String projectId) async {
+    return await repository.fetchProjectById(projectId);
   }
 }
--- lib/features/projects/domain/usercases/get_projects.dart
+++ lib/features/projects/domain/usercases/get_projects.dart
@@ -1,10 +1,11 @@
+import 'package:forge_recall/features/projects/domain/entities/project_entity.dart';
 import 'package:forge_recall/features/projects/domain/repositories/project_repository.dart';
 
 abstract class GetProjects {
   final ProjectRepository respository;
   GetProjects(this.respository);
 
-  Future<void> call(String userId) async {
-    respository.getProjects(userId);
+  Stream<List<ProjectEntity>> call(String userId) {
+    return respository.getProjects(userId);
   }
 }
